ca1c5f98dbd508cf311879250df761ef23e499b3
aburizalpurnama <purnamar9@gmail.com>
2021-08-06 11:43:36 +0700

    first commit

---
+++ src/test/java/com/rizalpurnama/belajar/spring/belajarspring/BelajarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizalpurnama.belajar.spring.belajarspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
